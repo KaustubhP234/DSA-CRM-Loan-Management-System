cd3a4f2493d126e6cbd5959612e486004a746741
Kaustubh Pawar <kaustubhpawar2021@gmail.com>
2026-02-04 14:24:54 +0530

    Fix all remaining case-sensitive component imports

--- src/pages/LoanApplication.jsx
+++ src/pages/LoanApplication.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import Footer from "../Components/Footer";
+import Footer from "../components/Footer";
 
 const LoanApplication = () => {
   const [form, setForm] = useState({
