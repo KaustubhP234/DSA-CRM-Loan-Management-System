159a35532955dad546d6d42bda4b3ee42abb7766
Kaustubh Pawar <kaustubhpawar2021@gmail.com>
2026-02-04 14:38:31 +0530

    Fix all Components casing globally using CLI

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import Navbar from "../Components/Navbar";
-import Footer from "../Components/Footer";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const Contact = () => {
   const [hoveredCard, setHoveredCard] = useState(null);
