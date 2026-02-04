c1a37928751cfe8229e35ff07889120406b9a7cf
Kaustubh Pawar <kaustubhpawar2021@gmail.com>
2026-02-04 14:35:03 +0530

    Fix all remaining case-sensitive component imports (final)

--- src/pages/HowItWorks.jsx
+++ src/pages/HowItWorks.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import Footer from "../Components/Footer";
-import Navbar from "../Components/Navbar";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 import processImg from "../assets/processimg.jpg";
 
 const HowItWorks = () => {
